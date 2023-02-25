1f7e11715c6ca431f85f1b508e7792ea2746ba60
BadDucck <66732726+BadDucck@users.noreply.github.com>
2023-02-25 16:44:58 +0300

    04 - Mini Oyun

---
+++ 04 - Mini Oyun/hafta13_2/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace hafta13_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn00 = new System.Windows.Forms.Button();
+            this.btn01 = new System.Windows.Forms.Button();
+            this.btn02 = new System.Windows.Forms.Button();
+            this.btn03 = new System.Windows.Forms.Button();
+            this.btn04 = new System.Windows.Forms.Button();
+            this.btn05 = new System.Windows.Forms.Button();
+            this.btn06 = new System.Windows.Forms.Button();
+            this.btn07 = new System.Windows.Forms.Button();
+            this.btn08 = new System.Windows.Forms.Button();
+            this.btn09 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn13 = new System.Windows.Forms.Button();
+            this.btn14 = new System.Windows.Forms.Button();
+            this.btn15 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn00
+            // 
+            this.btn00.Location = new System.Drawing.Point(254, 78);
+            this.btn00.Name = "btn00";
+            this.btn00.Size = new System.Drawing.Size(67, 67);
+            this.btn00.TabIndex = 0;
+            this.btn00.Text = "button1";
+            this.btn00.UseVisualStyleBackColor = true;
+            this.btn00.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn01
+            // 
+            this.btn01.Location = new System.Drawing.Point(327, 78);
+            this.btn01.Name = "btn01";
+            this.btn01.Size = new System.Drawing.Size(67, 67);
+            this.btn01.TabIndex = 1;
+            this.btn01.Text = "button2";
+            this.btn01.UseVisualStyleBackColor = true;
+            this.btn01.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn02
+            // 
+            this.btn02.Location = new System.Drawing.Point(400, 78);
+            this.btn02.Name = "btn02";
+            this.btn02.Size = new System.Drawing.Size(67, 67);
+            this.btn02.TabIndex = 2;
+            this.btn02.Text = "button3";
+            this.btn02.UseVisualStyleBackColor = true;
+            this.btn02.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn03
+            // 
+            this.btn03.Location = new System.Drawing.Point(473, 78);
+            this.btn03.Name = "btn03";
+            this.btn03.Size = new System.Drawing.Size(67, 67);
+            this.btn03.TabIndex = 3;
+            this.btn03.Text = "button4";
+            this.btn03.UseVisualStyleBackColor = true;
+            this.btn03.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btn04
+            // 
+            this.btn04.Location = new System.Drawing.Point(254, 151);
+            this.btn04.Name = "btn04";
+            this.btn04.Size = new System.Drawing.Size(67, 67);
+            this.btn04.TabIndex = 4;
+            this.btn04.Text = "button5";
+            this.btn04.UseVisualStyleBackColor = true;
+            this.btn04.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btn05
+            // 
+            this.btn05.Location = new System.Drawing.Point(327, 151);
+            this.btn05.Name = "btn05";
+            this.btn05.Size = new System.Drawing.Size(67, 67);
+            this.btn05.TabIndex = 5;
+            this.btn05.Text = "button6";
+            this.btn05.UseVisualStyleBackColor = true;
+            this.btn05.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // btn06
+            // 
+            this.btn06.Location = new System.Drawing.Point(404, 151);
+            this.btn06.Name = "btn06";
+            this.btn06.Size = new System.Drawing.Size(63, 67);
+            this.btn06.TabIndex = 6;
+            this.btn06.Text = "button7";
+            this.btn06.UseVisualStyleBackColor = true;
+            this.btn06.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // btn07
+            // 
+            this.btn07.Location = new System.Drawing.Point(474, 152);
+            this.btn07.Name = "btn07";
+            this.btn07.Size = new System.Drawing.Size(66, 66);
+            this.btn07.TabIndex = 7;
+            this.btn07.Text = "button8";
+            this.btn07.UseVisualStyleBackColor = true;
+            this.btn07.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // btn08
+            // 
+            this.btn08.Location = new System.Drawing.Point(254, 224);
+            this.btn08.Name = "btn08";
+            this.btn08.Size = new System.Drawing.Size(66, 66);
+            this.btn08.TabIndex = 15;
+            this.btn08.Text = "button9";
+            this.btn08.UseVisualStyleBackColor = true;
+            this.btn08.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // btn09
+            // 
+            this.btn09.Location = new System.Drawing.Point(331, 224);
+            this.btn09.Name = "btn09";
+            this.btn09.Size = new System.Drawing.Size(63, 67);
+            this.btn09.TabIndex = 14;
+            this.btn09.Text = "button10";
+            this.btn09.UseVisualStyleBackColor = true;
+            this.btn09.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // btn10
+            // 
+            this.btn10.Location = new System.Drawing.Point(400, 224);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(67, 67);
+            this.btn10.TabIndex = 13;
+            this.btn10.Text = "button11";
+            this.btn10.UseVisualStyleBackColor = true;
+            this.btn10.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // btn11
+            // 
+            this.btn11.Location = new System.Drawing.Point(473, 223);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(67, 67);
+            this.btn11.TabIndex = 12;
+            this.btn11.Text = "button12";
+            this.btn11.UseVisualStyleBackColor = true;
+            this.btn11.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // btn12
+            // 
+            this.btn12.Location = new System.Drawing.Point(254, 296);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(67, 67);
+            this.btn12.TabIndex = 11;
+            this.btn12.Text = "button13";
+            this.btn12.UseVisualStyleBackColor = true;
+            this.btn12.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // btn13
+            // 
+            this.btn13.Location = new System.Drawing.Point(327, 297);
+            this.btn13.Name = "btn13";
+            this.btn13.Size = new System.Drawing.Size(67, 67);
+            this.btn13.TabIndex = 10;
+            this.btn13.Text = "button14";
+            this.btn13.UseVisualStyleBackColor = true;
+            this.btn13.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // btn14
+            // 
+            this.btn14.Location = new System.Drawing.Point(400, 297);
+            this.btn14.Name = "btn14";
+            this.btn14.Size = new System.Drawing.Size(67, 67);
+            this.btn14.TabIndex = 9;
+            this.btn14.Text = "button15";
+            this.btn14.UseVisualStyleBackColor = true;
+            this.btn14.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // btn15
+            // 
+            this.btn15.Location = new System.Drawing.Point(473, 296);
+            this.btn15.Name = "btn15";
+            this.btn15.Size = new System.Drawing.Size(67, 67);
+            this.btn15.TabIndex = 8;
+            this.btn15.Text = "button16";
+            this.btn15.UseVisualStyleBackColor = true;
+            this.btn15.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn08);
+            this.Controls.Add(this.btn09);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn13);
+            this.Controls.Add(this.btn14);
+            this.Controls.Add(this.btn15);
+            this.Controls.Add(this.btn07);
+            this.Controls.Add(this.btn06);
+            this.Controls.Add(this.btn05);
+            this.Controls.Add(this.btn04);
+            this.Controls.Add(this.btn03);
+            this.Controls.Add(this.btn02);
+            this.Controls.Add(this.btn01);
+            this.Controls.Add(this.btn00);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn00;
+        private System.Windows.Forms.Button btn01;
+        private System.Windows.Forms.Button btn02;
+        private System.Windows.Forms.Button btn03;
+        private System.Windows.Forms.Button btn04;
+        private System.Windows.Forms.Button btn05;
+        private System.Windows.Forms.Button btn06;
+        private System.Windows.Forms.Button btn07;
+        private System.Windows.Forms.Button btn08;
+        private System.Windows.Forms.Button btn09;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn13;
+        private System.Windows.Forms.Button btn14;
+        private System.Windows.Forms.Button btn15;
+    }
+}
+
---
+++ 04 - Mini Oyun/hafta13_2/Form1.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hafta13_2
+{
+    public partial class Form1 : Form
+    {
+        int[,] tahta = new int[4, 4];
+
+        void degistir(int satir, int sutun)
+        {
+            if (tahta[satir,sutun]==1)
+       
+            tahta[satir, sutun] = 0;
+            else
+                tahta[satir, sutun] = 1;
+
+            if(satir + 1 <= 3) {
+
+                if (tahta[satir + 1, sutun] == 1)
+
+                    tahta[satir + 1, sutun] = 0;
+                else
+                    tahta[satir + 1, sutun] = 1;
+            }
+
+            if (satir - 1 >= 0)
+            {
+
+                if (tahta[satir - 1, sutun] == 1)
+
+                    tahta[satir - 1, sutun] = 0;
+                else
+                    tahta[satir - 1, sutun] = 1;
+            }
+
+            if (sutun + 1 <= 3)
+            {
+                if(tahta[satir , sutun+1] == 1)
+
+                    tahta[satir , sutun+1] = 0;
+                else
+                    tahta[satir, sutun+1] = 1;
+            }
+
+            if (sutun - 1 >=0)
+            {
+                if (tahta[satir, sutun - 1] == 1)
+
+                    tahta[satir, sutun - 1] = 0;
+                else
+                    tahta[satir, sutun - 1] = 1;
+            }
+
+            
+
+
+        }
+        void boya()
+        {
+            /*Btn00*/
+            if (tahta[0,0] == 0)
+            {
+                btn00.BackColor = Color.Black;
+            }
+            else
+            {
+                btn00.BackColor = Color.White;
+            }
+
+            /*Btn01*/
+            if (tahta[0, 1] == 0)
+            {
+                btn01.BackColor = Color.Black;
+            }
+            else
+            {
+                btn01.BackColor = Color.White;
+            }
+
+            /*Btn02*/
+            if (tahta[0, 2] == 0)
+            {
+                btn02.BackColor = Color.Black;
+            }
+            else
+            {
+                btn02.BackColor = Color.White;
+            }
+
+            /*Btn03*/
+            if (tahta[0, 3] == 0)
+            {
+                btn03.BackColor = Color.Black;
+            }
+            else
+            {
+                btn03.BackColor = Color.White;
+            }
+
+            /*Btn04*/
+            if (tahta[1, 0] == 0)
+            {
+                btn04.BackColor = Color.Black;
+            }
+            else
+            {
+                btn04.BackColor = Color.White;
+            }
+
+            /*Btn05*/
+            if (tahta[1, 1] == 0)
+            {
+                btn05.BackColor = Color.Black;
+            }
+            else
+            {
+                btn05.BackColor = Color.White;
+            }
+
+            /*Btn06*/
+            if (tahta[1, 2] == 0)
+            {
+                btn06.BackColor = Color.Black;
+            }
+            else
+            {
+                btn06.BackColor = Color.White;
+            }
+
+            /*Btn07*/
+            if (tahta[1, 3] == 0)
+            {
+                btn07.BackColor = Color.Black;
+            }
+            else
+            {
+                btn07.BackColor = Color.White;
+            }
+
+            /*Btn08*/
+            if (tahta[2, 0] == 0)
+            {
+                btn08.BackColor = Color.Black;
+            }
+            else
+            {
+                btn08.BackColor = Color.White;
+            }
+
+            /*btn09*/
+            if (tahta[2, 1] == 0)
+            {
+                btn09.BackColor = Color.Black;
+            }
+            else
+            {
+                btn09.BackColor = Color.White;
+            }
+
+            /*Btn10*/
+            if (tahta[2, 2] == 0)
+            {
+                btn10.BackColor = Color.Black;
+            }
+            else
+            {
+                btn10.BackColor = Color.White;
+            }
+
+            /*Btn11*/
+            if (tahta[2, 3] == 0)
+            {
+                btn11.BackColor = Color.Black;
+            }
+            else
+            {
+                btn11.BackColor = Color.White;
+            }
+
+            /*Btn12*/
+            if (tahta[3, 0] == 0)
+            {
+                btn12.BackColor = Color.Black;
+            }
+            else
+            {
+                btn12.BackColor = Color.White;
+            }
+
+            /*Btn13*/
+            if (tahta[3, 1] == 0)
+            {
+                btn13.BackColor = Color.Black;
+            }
+            else
+            {
+                btn13.BackColor = Color.White;
+            }
+
+            /*Btn14*/
+            if (tahta[3, 2] == 0)
+            {
+                btn14.BackColor = Color.Black;
+            }
+            else
+            {
+                btn14.BackColor = Color.White;
+            }
+
+            /*Btn15*/
+            if (tahta[3, 3] == 0)
+            {
+                btn15.BackColor = Color.Black;
+            }
+            else
+            {
+                btn15.BackColor = Color.White;
+            }
+
+
+            int toplam = 0;
+            for(int i =0; i < 4; i++)
+           
+                for(int j=0; j< 4 ; j++)
+                {
+                    toplam = tahta[i,j] + toplam;
+                }
+            
+            if (toplam == 16)
+            {
+                MessageBox.Show("KAZANDINIZ");
+            }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tahta[0, 0] = 1;
+            tahta[0, 3] = 1;
+            tahta[3, 0] = 1;
+            tahta[3, 3] = 1;
+
+  
+            boya();
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            degistir(2, 2);
+            boya();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            degistir(2, 0);
+            boya();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            degistir(2, 1);
+            boya();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            degistir(2, 3);
+            boya();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            degistir(3, 0);
+            boya();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            degistir(3, 1);
+            boya();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            degistir(3, 2);
+            boya();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            degistir(3, 3);
+            boya();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            degistir(1, 3);
+            boya();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            degistir(1, 2);
+            boya();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            degistir(1, 1);
+            boya();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            degistir(1, 0);
+            boya();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            degistir(0, 3);
+            boya();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            degistir(0, 2);
+            boya();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            degistir(0, 1);
+            boya();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            degistir(0, 0);
+            boya();
+        }
+    }
+}
