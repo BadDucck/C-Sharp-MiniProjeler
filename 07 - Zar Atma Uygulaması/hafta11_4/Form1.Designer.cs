a8ead7765bf646b81cc512e9114ab7edd2764978
BadDucck <66732726+BadDucck@users.noreply.github.com>
2023-02-28 00:19:26 +0300

    07 - Zar Atma Uygulaması

---
+++ 07 - Zar Atma Uygulaması/hafta11_4/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace hafta11_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imList = new System.Windows.Forms.ImageList(this.components);
+            this.pcb1 = new System.Windows.Forms.PictureBox();
+            this.pcb2 = new System.Windows.Forms.PictureBox();
+            this.btnZarAt = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imList
+            // 
+            this.imList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imList.ImageStream")));
+            this.imList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imList.Images.SetKeyName(0, "donen_zar.gif");
+            this.imList.Images.SetKeyName(1, "zar_1.png");
+            this.imList.Images.SetKeyName(2, "zar_2.png");
+            this.imList.Images.SetKeyName(3, "zar_3.png");
+            this.imList.Images.SetKeyName(4, "zar_4.png");
+            this.imList.Images.SetKeyName(5, "zar_5.png");
+            this.imList.Images.SetKeyName(6, "zar_6.png");
+            // 
+            // pcb1
+            // 
+            this.pcb1.Location = new System.Drawing.Point(88, 56);
+            this.pcb1.Name = "pcb1";
+            this.pcb1.Size = new System.Drawing.Size(272, 251);
+            this.pcb1.TabIndex = 0;
+            this.pcb1.TabStop = false;
+            // 
+            // pcb2
+            // 
+            this.pcb2.Location = new System.Drawing.Point(440, 56);
+            this.pcb2.Name = "pcb2";
+            this.pcb2.Size = new System.Drawing.Size(272, 251);
+            this.pcb2.TabIndex = 1;
+            this.pcb2.TabStop = false;
+            // 
+            // btnZarAt
+            // 
+            this.btnZarAt.Location = new System.Drawing.Point(329, 351);
+            this.btnZarAt.Name = "btnZarAt";
+            this.btnZarAt.Size = new System.Drawing.Size(137, 51);
+            this.btnZarAt.TabIndex = 2;
+            this.btnZarAt.Text = "Zar At";
+            this.btnZarAt.UseVisualStyleBackColor = true;
+            this.btnZarAt.Click += new System.EventHandler(this.btnZarAt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnZarAt);
+            this.Controls.Add(this.pcb2);
+            this.Controls.Add(this.pcb1);
+            this.Name = "Form1";
+            this.Text = "Zar at";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imList;
+        private System.Windows.Forms.PictureBox pcb1;
+        private System.Windows.Forms.PictureBox pcb2;
+        private System.Windows.Forms.Button btnZarAt;
+    }
+}
+
---
+++ 07 - Zar Atma Uygulaması/hafta11_4/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hafta11_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pcb1.Image = imList.Images[0];
+            pcb2.Image = imList.Images[0];
+        }
+
+        private void btnZarAt_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            pcb1.Image = imList.Images[r.Next(1,6)];
+            
+            pcb2.Image = imList.Images[r.Next(1, 6)];
+        }
+    }
+}
