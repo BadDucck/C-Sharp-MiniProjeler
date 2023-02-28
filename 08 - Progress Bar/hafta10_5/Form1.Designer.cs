09a297b890b69ebc8d68ad62e7e6bd64cdcb465d
BadDucck <66732726+BadDucck@users.noreply.github.com>
2023-02-28 22:53:59 +0300

    08 - Progress Bar

---
+++ 08 - Progress Bar/hafta10_5/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace hafta10_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblYazılım = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(173, 183);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(456, 57);
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.Value = 50;
+            // 
+            // lblYazılım
+            // 
+            this.lblYazılım.AutoSize = true;
+            this.lblYazılım.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYazılım.Location = new System.Drawing.Point(168, 136);
+            this.lblYazılım.Name = "lblYazılım";
+            this.lblYazılım.Size = new System.Drawing.Size(242, 29);
+            this.lblYazılım.TabIndex = 1;
+            this.lblYazılım.Text = "Yazılım Yükleniyor...";
+            this.lblYazılım.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblYazılım.Click += new System.EventHandler(this.lblYazılım_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblYazılım);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblYazılım;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 08 - Progress Bar/hafta10_5/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hafta10_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void lblYazılım_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            progressBar1.Value+=5;
+            lblYazılım.Text=Convert.ToString("Yazılım Yükleniyor %" + progressBar1.Value);
+            if (progressBar1.Value == 100)
+            {
+                timer1.Stop();
+                lblYazılım.Text = "İndirme işlemi tamamlandı.";
+            }
+        }
+    }
+}
